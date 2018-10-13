ec06923822d0231ed36764d30bc629ab56f5eb2a
13Legend31 <37526178+13Legend31@users.noreply.github.com>
2018-10-13 13:24:46 -0400

    asdf

--- service/routes/session.js
+++ service/routes/session.js
@@ -1,16 +1,9 @@
 module.exports = function (app, io, workspaceApi, storage, request, statisticsApi, provisioningApi) {
 
-  function loginWithLoginPage(req,res) {
-    storage.redirectUri = `${req.protocol}://${req.hostname}:${storage.port}/initialize`;
-    const authLoginPage = `${storage.apiUrl}/auth/v3/oauth/authorize?response_type=code&client_id=${storage.clientId}&redirect_uri=${storage.redirectUri}`;
-    res.redirect(authLoginPage);
-  };
-
   function loginWithoutLoginPage(req,res) {
+    console.log('AUTHENTICATION COMPLETE')
     let encodedCredentials = new Buffer(`${storage.clientId}:${storage.clientSecret}`).toString('base64');
-    // Your agent username
     let username = 'StephaneHervochon@genesys.com';
-    // Your agent password
     let password = 'Genesys2!';
     request.post(`${storage.apiUrl}/auth/v3/oauth/token`, {
       headers: {
@@ -32,14 +25,13 @@ module.exports = function (app, io, workspaceApi, storage, request, statisticsAp
   };
 
   function initializeWorkspace(body, res) {
+    console.log('INITIALIZATION COMPLETE')
     storage.token = body.access_token;
     workspaceApi.initialize({token: storage.token}).then(() => {
         workspaceApi.activateChannels(workspaceApi.user.employeeId, null, workspaceApi.user.defaultPlace).then(() => {
           storage.user = workspaceApi.user;
-          statisticsApi.initialize(storage.token).then( () => {
-            provisioningApi.initialize({token: storage.token}).then( () => {
-              res.redirect('/authenticated');
-            });
+          statisticsApi.initialize(storage.token).then(() => {
+            provisioningApi.initialize({token: storage.token})
           });
         })
         .catch(err => {
@@ -59,7 +51,6 @@ module.exports = function (app, io, workspaceApi, storage, request, statisticsAp
     }
   });
   
-  // Initializing Workspace API
   app.get('/initialize', (req, res) => {
     let encodedCredentials = new Buffer(`${storage.clientId}:${storage.clientSecret}`).toString('base64');
     request.post(`${storage.apiUrl}/auth/v3/oauth/token`, {
@@ -79,15 +70,10 @@ module.exports = function (app, io, workspaceApi, storage, request, statisticsAp
      });
   });
 
-  // Logging in (redirect to auth page)
   app.get('/login', (req, res) => {
-    //To log in with a login page
-    loginWithLoginPage(req,res);
-    //To log in without a login page (userName & password available in the request to get auth token)
-    //loginWithoutLoginPage(req,res);
+    loginWithoutLoginPage(req,res);
   });
 
-  // Logout and redirect to home
   app.get('/logout', (req, res) => {
     workspaceApi.destroy();
     storage.user = null;
